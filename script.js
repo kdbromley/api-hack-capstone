299404129c370566abe8ad53f258237a7d156335
kd Bromley <kd.bromley1@gmail.com>
2020-12-11 12:18:54 -0800

    remove hardcode, ready to test whole app

--- script.js
+++ script.js
@@ -1,163 +1,5 @@
 'use strict';
 
-const responseJsonR = {
-    "results": [
-        {
-            "id": 667024,
-            "title": "Simple Spaghetti Fra Diavolo",
-            "readyInMinutes": 45,
-            "servings": 4,
-            "sourceUrl": "http://bakerbynature.com/simple-spaghetti-fra-diavolo/",
-            "openLicense": 0,
-            "image": "Simple-Spaghetti-Fra-Diavolo-667024.jpg"
-        },
-        {
-            "id": 718828,
-            "title": "21 Day Fix Broccoli Mac & Cheese (AKA the perfect Mac & Cheese)",
-            "readyInMinutes": 45,
-            "servings": 8,
-            "sourceUrl": "http://thefoodieandthefix.com/21-day-fix-broccoli-mac-cheese-aka-the-perfect-mac-cheese/",
-            "openLicense": 0,
-            "image": "21-day-fix-broccoli-mac-cheese-(aka-the-perfect-mac-cheese)-718828.jpg"
-        },
-        {
-            "id": 603329,
-            "title": "Avocado Pasta",
-            "readyInMinutes": 20,
-            "servings": 4,
-            "sourceUrl": "http://damndelicious.net/2014/06/20/avocado-pasta/",
-            "openLicense": 0,
-            "image": "Avocado-Pasta-603329.jpg"
-        },
-        {
-            "id": 694932,
-            "title": "Ravioli & Vegetable Soup",
-            "readyInMinutes": 25,
-            "servings": 4,
-            "sourceUrl": "http://www.eatingwell.com/recipes/ravioli_vegetable_soup.html",
-            "openLicense": 0,
-            "image": "ravioli-vegetable-soup-694932.jpg"
-        },
-        {
-            "id": 735844,
-            "title": "Creamy Lemon Pasta",
-            "readyInMinutes": 30,
-            "servings": 6,
-            "sourceUrl": "http://cooking.nytimes.com/recipes/8355-creamy-lemon-pasta",
-            "openLicense": 0,
-            "image": "creamy-lemon-pasta-735844.jpg"
-        },
-        {
-            "id": 621163,
-            "title": "Pesto Pasta with Lemon, Spinach, Edamame & Toasted Almonds",
-            "readyInMinutes": 45,
-            "servings": 4,
-            "sourceUrl": "http://www.thekitchn.com/recipe-pesto-pasta-with-lemon-145783",
-            "openLicense": 0,
-            "image": "Pesto-Pasta-with-Lemon--Spinach--Edamame---Toasted-Almonds-621163.jpg"
-        },
-        {
-            "id": 750949,
-            "title": "Mexican Macaroni Salad",
-            "readyInMinutes": 60,
-            "servings": 12,
-            "sourceUrl": "http://www.foodnetwork.com/recipes/ree-drummond/mexican-macaroni-salad.html",
-            "openLicense": 0,
-            "image": "mexican-macaroni-salad-750949.jpeg"
-        },
-        {
-            "id": 668358,
-            "title": "Spaghetti with Avocado Sauce",
-            "readyInMinutes": 30,
-            "servings": 8,
-            "sourceUrl": "http://www.purewow.com/recipes/Spaghetti-with-Avocado-Sauce?utm_source=zergnet",
-            "openLicense": 0,
-            "image": "Spaghetti-with-Avocado-Sauce-668358.png"
-        },
-        {
-            "id": 586792,
-            "title": "Poblano Macaroni and Cheese",
-            "readyInMinutes": 60,
-            "servings": 15,
-            "sourceUrl": "http://www.nutmegnanny.com/2011/09/10/poblano-macaroni-and-cheese-the-homesick-texan-cookbook-spotlight-cook-off/",
-            "openLicense": 0,
-            "image": "Poblano-Macaroni-and-Cheese-586792.jpg"
-        },
-        {
-            "id": 577319,
-            "title": "Caprese Pasta Salad",
-            "readyInMinutes": 35,
-            "servings": 6,
-            "sourceUrl": "http://www.simplyscratch.com/2013/07/caprese-pasta-salad.html",
-            "openLicense": 0,
-            "image": "Caprese-Pasta-Salad-577319.jpg"
-        }
-    ],
-    "baseUri": "https://spoonacular.com/recipeImages/",
-    "offset": 0,
-    "number": 10,
-    "totalResults": 806,
-    "processingTimeMs": 538,
-    "expires": 1607920554934
-};
-const responseJsonDrink =
-    { drinks:  
-        [ { idDrink: '16986',
-            strDrink: 'Bible Belt',
-            strDrinkAlternate: null,
-            strDrinkES: null,
-            strDrinkDE: null,
-            strDrinkFR: null,
-            'strDrinkZH-HANS': null,
-            'strDrinkZH-HANT': null,
-            strTags: null,
-            strVideo: null,
-            strCategory: 'Other/Unknown',
-            strIBA: null,
-            strAlcoholic: 'Alcoholic',
-            strGlass: 'Highball glass',
-            strInstructions: 'Mix all ingredients, and pour over ice.',
-            strInstructionsES: null,
-            strInstructionsDE: 'Alle Zutaten mischen und über Eis gießen.',
-            strInstructionsFR: null,
-            'strInstructionsZH-HANS': null,
-            'strInstructionsZH-HANT': null,
-            strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/6bec6v1503563675.jpg',
-            strIngredient1: 'Southern Comfort',
-            strIngredient2: 'Triple sec',
-            strIngredient3: 'Lime',
-            strIngredient4: 'Sour mix',
-            strIngredient5: null,
-            strIngredient6: null,
-            strIngredient7: null,
-            strIngredient8: null,
-            strIngredient9: null,
-            strIngredient10: null,
-            strIngredient11: null,
-            strIngredient12: null,
-            strIngredient13: null,
-            strIngredient14: null,
-            strIngredient15: null,
-            strMeasure1: '2 oz ',
-            strMeasure2: '1/2 oz ',
-            strMeasure3: '2 wedges ',
-            strMeasure4: '2 oz ',
-            strMeasure5: null,
-            strMeasure6: null,
-            strMeasure7: null,
-            strMeasure8: null,
-            strMeasure9: null,
-            strMeasure10: null,
-            strMeasure11: null,
-            strMeasure12: null,
-            strMeasure13: null,
-            strMeasure14: null,
-            strMeasure15: null,
-            strImageSource: null,
-            strImageAttribution: null,
-            strCreativeCommonsConfirmed: 'No',
-            dateModified: '2017-08-24 09:34:35' } ] 
-        };
             
 const apiKey = '';
 const searchURLRecipe= 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/'
@@ -165,30 +7,30 @@ const recipeEndpoint = 'recipes/search?'
 const searchURLCocktail = 'https://the-cocktail-db.p.rapidapi.com/'
 const cocktailRandomEndpoint = 'random.php'
 
-function generateDrinkLink(responseJsonDrink) {
+function generateDrinkLink(responseJson) {
     const liveURL = 'https://thecocktaildb.com/drink/'
-    const drinkId = responseJsonDrink.drinks[0].idDrink
-    const uriDrink = encodeURIComponent(responseJsonDrink.drinks[0].strDrink).replace(/%20/g, '-');
+    const drinkId = responseJson.drinks[0].idDrink
+    const uriDrink = encodeURIComponent(responseJson.drinks[0].strDrink).replace(/%20/g, '-');
     return liveURL + drinkId + '-' + uriDrink
   }
 
-function displayCocktailRec(responseJsonDrink) {
-    console.log('displayCocktailRec ran')
-    const drinkLink = generateDrinkLink(responseJsonDrink)
+function displayCocktailRec(responseJson) {
+    console.log(responseJson)
+    const drinkLink = generateDrinkLink(responseJson)
     console.log(drinkLink)
-    $('.js-recipe-list').find('section.bev-options').html(`<p class="random-drink-result">Try a <a href=${drinkLink} target="_blank">${responseJsonDrink.drinks[0].strDrink}!</p>`)
+    $('.js-recipe-list').find('section.bev-options').html(`<p class="random-drink-result">Try a <a href=${drinkLink} target="_blank">${responseJson.drinks[0].strDrink}!</p>`)
 }
 
 function getCocktailRec() {
     console.log('getCocktailRec ran')
     const url = searchURLCocktail + cocktailRandomEndpoint
     console.log(url)
-  /* fetch(url, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "8ab972b41cmshe8e107ca90c05d2p1a2d95jsnfe7a217fe61a",
-		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com"
-	}
+    fetch(url, {
+	    "method": "GET",
+	    "headers": {
+		    "x-rapidapi-key": apiKey,
+		    "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com"
+	    }
     })
     .then(response => { 
       if (response.ok) {
@@ -197,9 +39,9 @@ function getCocktailRec() {
       throw new Error(response.statusText)
     })
     .then(responseJson => displayCocktailRec(responseJson))
-    .catch(err => { alert(err);
-    }); */
-    displayCocktailRec(responseJsonDrink)
+    .catch(err => { alert(`Uh oh! An error occured: ${err.message}`);
+    });
+
 }
 
 
@@ -217,16 +59,16 @@ function displayDrinkOptions() {
 }
 
 
-function displayRecipeResults(responseJsonR) {
-    //console.log(responseJsonR);
+function displayRecipeResults(responseJson) {
+    console.log(responseJson);
     $('.js-error-message').text('');
     $('.js-recipe-list').empty();
-    for (let i = 0; i < responseJsonR.results.length; i++) {
+    for (let i = 0; i < responseJson.results.length; i++) {
         $('.js-recipe-list').append(`
-        <li><h4><a href=${responseJsonR.results[i].url}>${responseJsonR.results[i].title}</a></h4>
-        <img href="${responseJsonR.results[i].image}" alt="${responseJsonR.results[i].title}">
-        <p>Ready in ${responseJsonR.results[i].readyInMinutes} minutes! Makes ${responseJsonR.results[i].servings} servings</p>
-        <p>Source recipe:<a href=${responseJsonR.results[i].sourceUrl}>${responseJsonR.results[i].sourceUrl}</a></p>
+        <li><h4><a href=${responseJson.results[i].url}>${responseJson.results[i].title}</a></h4>
+        <img href="${responseJson.results[i].image}" alt="${responseJson.results[i].title}">
+        <p>Ready in ${responseJson.results[i].readyInMinutes} minutes! Makes ${responseJson.results[i].servings} servings</p>
+        <p>Source recipe:<a href=${responseJson.results[i].sourceUrl}>${responseJson.results[i].sourceUrl}</a></p>
         ${displayDrinkOptions()}
        </li>`)
     }
@@ -253,22 +95,23 @@ function getRecipeList(recipeQuery, intoleranceInput, dietInput, numberInput=10)
     const queryString = formatQueryParams(recipeParams);
     const url = searchURLRecipe + recipeEndpoint + queryString;
     console.log(url);
-    /* fetch(url, {
-	"headers": {
-		"x-rapidapi-key": apiKey,
-		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-	})
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response.statusText);
-        })
-        .then(responseJson => {
-            console.log(responseJson)
-        })
-        */
-        displayRecipeResults(responseJsonR);
+    fetch(url, {
+	    "headers": {
+		    "x-rapidapi-key": apiKey,
+		    "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+         }
+        throw new Error(response.statusText);
+     })
+    .then(responseJson => displayRecipeResults(responseJson))
+    .catch(err => { alert(`Uh oh! An error occured: ${err.message}`);
+        });
+
+        displayRecipeResults(responseJson);
 }
 
 
